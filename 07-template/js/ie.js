aeac88377e9fe59c3a6810020732d944e9f1b13f
kimhwidam <emddjr178@gmail.com>
2023-07-17 15:07:29 +0900

    0717 profile practice

--- 07-template/js/ie.js
+++ 07-template/js/ie.js
@@ -4,6 +4,10 @@ console.log(ver);
 const isIE = /trident/i.test(ver);
 console.log(isIE);
 
-if(isIE){
-  alert("익스플로러 브라우저로 접속하셨네요. 이 웹 페이지는 익스플로러를 지원하지 않습니다. 다른 브라우저로 접속해주세요.")
+if (isIE) {
+  alert(
+    "익스플로러 브라우저로 접속하셨네요. 이 웹 페이지는 익스플로러를 지원하지 않습니다. 다른 브라우저로 접속해주세요."
+  );
 }
+
+alert("김휘담 님의 프로필 카드입니다.");
